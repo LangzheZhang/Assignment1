b147768cbc113f4a31c3108f4872249a5af31360
Langzhe Zhang <605495433@qq.com>
2020-11-17 11:14:08 +0800

    finish the list of member show a table

--- app/Controllers/NINEPERCENT.php
+++ app/Controllers/NINEPERCENT.php
@@ -8,13 +8,42 @@ class NINEPERCENT extends BaseController
     $member = new \App\Models\Member();
         // retrieve all the records
     $records = $member->findAll();
+
+    $table = new \CodeIgniter\View\Table();
     
+    $headings = $member->fields;
+    $table->setHeading($headings[1],$headings[7],"details");
+    foreach ($records as $record) {
+             $nameLink = anchor("ninepercent/showme/$record->id",$record->name);
+            
+            $table->addRow(
+                    $record->name,"<img src=\"/image/".$record->image."\"/>",
+                    anchor("NINEPERCENT/showme/$record->id", $record->company)
+            );
+    }$template = [
+        'table_open' => '<table cellpadding="5px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">',
+     ];
+    $table->setTemplate($template);
+    $fields = [
+         'title' => 'Member',
+         'heading' => 'Member',
+         'footer' => 'Langzhe Zhang'
+         ];
+
+
         // get a template parser
     $parser = \Config\Services::parser();
         // tell it about the substitions
-    return $parser->setData(['records' => $records])
+   /* return $parser->setData(['records' => $records])
         // and have it render the template with those
-    ->render('memberlist');
+    ->render('placeslist');*/
+return $parser->setData($fields)
+               ->render('templates\top') .
+       $table->generate() .
+        $parser->setData($fields)
+               ->render('templates\bottom');
     }
     public function showme($id)
     {
